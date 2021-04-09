9cf66345a447982ddc31771c35bc1df18498a52a
leeyouyi <a0986599501@gmail.com>
2021-04-09 17:46:15 +0800

    202104091746

--- canvas/js/index.js
+++ canvas/js/index.js
@@ -10,6 +10,7 @@
         width: configWith,
         height: configHeight,
         parent: "canvas",
+        backgroundColor: '#00ff00',
         physics: {
             default: 'arcade',
             arcade: {
@@ -24,9 +25,6 @@
     };
 
     var game = new Phaser.Game(config);
-    var curve = [] 
-    var graphics =[]
-    var path =[]
     var inputAry = []
     var _this 
     var end = true
@@ -36,25 +34,18 @@
     var newAry= []
     var sortAry = []
     var text1
-    var text2
 
 
     document.querySelector('#btn').addEventListener('click',()=>{
         location.reload()
     })
-    document.querySelector('#btn2').addEventListener('click',()=>{
-        for (let i = 0; i < 7; i++) {
-            start(i + 1, i)
-            inputValue.push(i+1)
-        }
+    // document.querySelector('#btn2').addEventListener('click',()=>{
+    //     for (let i = 0; i < 7; i++) {
+    //         start(i + 1, i)
+    //         inputValue.push(i+1)
+    //     }
      
-    })
-    function getDegree(x1, y1, x2, y2) {
-        var x = Math.abs(x1 - x2);
-        var y = Math.abs(y1 - y2);
-        var z = Math.sqrt(x * x + y * y);
-        return Math.round((Math.asin(y / z) / Math.PI * 180));
-    }
+    // })
 
     function preload ()
     {
@@ -66,10 +57,6 @@
             this.load.image(`ball${i}`, `./assets/Ball/${num}.png`)
         }
         
-        // this.load.spritesheet('10', './assets/10.png', {
-        //     frameWidth: 500,
-        //     frameHeight: 500
-        // });
 
         this.load.spritesheet('Pad', './assets/Pad.png', {
             frameWidth: 1024,
@@ -115,7 +102,74 @@
             frameHeight: 500
         });
 
+        this.load.spritesheet('BF10', './assets/balls/BF10.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
 
+        this.load.spritesheet('BF16', './assets/balls/BF16.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('BF22', './assets/balls/BF22.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('BF40', './assets/balls/BF40.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('BF44', './assets/balls/BF44.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('BF45', './assets/balls/BF45.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('BF49', './assets/balls/BF49.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+        this.load.spritesheet('FB10', './assets/balls/FB10.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('FB16', './assets/balls/FB16.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('FB22', './assets/balls/FB22.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('FB40', './assets/balls/FB40.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('FB44', './assets/balls/FB44.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('FB45', './assets/balls/FB45.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
+
+        this.load.spritesheet('FB49', './assets/balls/FB49.png', {
+            frameWidth: 500,
+            frameHeight: 500
+        });
     }
 
 
@@ -129,10 +183,8 @@
         _this = this
         this.num = 0
         this.ball = []
-        // this.panel =  this.add.image(682 , 600, `Panel`).setScale(.35).setDepth(1)
         this.panel = this.add.sprite(0,0, 'Pad' ).setScale(1.40625).setDepth(1).setOrigin(0.0).setFrame(0)
-        // this.Z = .35
-        // this.Y = true
+
         this.R = .5
         this.go = true
 
@@ -144,61 +196,55 @@
             }),
             frameRate: 15,
         });
+        this.BFball= []
+        this.FBball= []
+        let no=''
 
+        for (let i = 0; i < 7; i++) {
+            let distance = i < 7 ? (i)* 123 :  (i)* 123 + 18
+            switch (i) {
+                case 0: no = '10'
+                    
+                    break;
+                case 1: no = '16'
+                    
+                    break;
+                case 2: no = '22'
+                    
+                    break;
+                case 3: no = '40'
+                    
+                    break;
+                case 4: no = '44'
+                    
+                    break;
+                case 5: no = '45'
+                    
+                    break;
+                case 6: no = '49'
+                    
+                    break;
+    
+            }
+            this.anims.create({
+                key: 'BFball'+ no ,
+                frames: this.anims.generateFrameNumbers('BF'+ no, {
+                    start: 0,
+                    end: 14
+                }),
+                frameRate: 15,
+            });
+            this.anims.create({
+                key: 'FBball'+ no ,
+                frames: this.anims.generateFrameNumbers('FB'+ no, {
+                    start: 0,
+                    end: 14
+                }),
+                frameRate: 15,
+            });
+            // this.FBball[i] = this.add.sprite(340 + distance, 700, `FB${no}` ).setScale(.25).setDepth(0).setFrame(6)
+        }
 
-        // this.test=[]
-        // this.test2=[]
-
-        // this.panel2 = this.add.sprite(0,-400, 'Pad' ).setScale(1.40625).setDepth(1).setOrigin(0.0).setFrame(0)
-
-        // this.anims.create({
-        //     key: 'rotat2' ,
-        //     frames: this.anims.generateFrameNumbers('Pad', {
-        //         start: 0,
-        //         end: 14
-        //     }),
-        //     frameRate: 15,
-        //     // repeat: -1,
-
-        // });
-        // this.panel2.play('rotat2')
-     
-        // let n =0
-        // let j = 0
-        // for (let i = 0; i < 7; i++) {
-        //     i == 6 ? j = 5  : j = 0
-        //     this.test[i] = this.add.sprite(340 + n , 260 + j, 'No10' ).setScale(.25).setDepth(2)
-        //     this.test2[i] = this.add.sprite(340 + n , 260 + j, 'No10' ).setScale(.25).setDepth(0)
-        //     i < 5 ?  n = n + 123 : n = n  + 140
-        // }
-
-    //     this.anims.create({
-    //         key: 'ball0' ,
-    //         frames: this.anims.generateFrameNumbers('No10', {
-    //             start: 0,
-    //             end: 29
-    //         }),
-    //         frameRate: 30,
-    //         repeat: -1
-    //     });
-
-    //    b.play('ball0')
-
-        // b.setScale(.25, .2) 
-
-
-    //    let f = this.add.sprite(200,110, 'fish1' ).setDepth(2)
-    //     this.anims.create({
-    //         key: 'fish' ,
-    //         frames: this.anims.generateFrameNumbers('fish1', {
-    //             start: 0,
-    //             end: 14
-    //         }),
-    //         frameRate: 30,
-    //         repeat: -1
-    //     });
-
-    //     f.play('fish')
 
 
         var inputBox = document.querySelector("#inputBox")
@@ -213,12 +259,10 @@
                     if( Number(value) <= 0  || (Number(value) > 49) ) return
                     if(hasValue !== -1) return
                     if(end && (nextNum === index + 1)){
-                        // console.log(hasValue)
                         inputValue.push(value)
                         start(index + 1, value)
                         this.num = index + 1
                         nextNum = nextNum + 1
-                        // console.log(inputValue)
                         document.querySelector('#numAry').innerText = inputValue
                     }
                 
@@ -229,7 +273,6 @@
 
         for (let i = 0; i < 8; i++) {
             inputAry[i] = true
-            
         }
 
 
@@ -240,42 +283,39 @@
  
         let no = ''
         let Offset = 0 
+
         switch (i) {
             case 1: no = '10'
-                    Offset = -200
+                    Offset = -150
                 break;
             case 2: no = '45'
-                    Offset = 200
-                
+                    Offset = 150
                 break;
             case 3: no = '22'
-                    Offset = -200
+                    Offset = -150
                 break;
             case 4: no = '40'
-                    Offset = 200
-                
+                    Offset = 150
                 break;
             case 5: no = '44'
-                    Offset = -200
-                
+                    Offset = -150
                 break;
             case 6: no = '16'
-                    Offset = 200
-                
+                    Offset = 150
                 break;
             case 7: no = '49'
-                    Offset = 200
+                    Offset = 150
                 break;
 
         }
 
         if(inputAry[i]){
             
-            let distance = i < 7 ? (i -1)* 123.5 :  (i-1)* 123 + 18
- 
+            let distance = i < 7 ? (i -1)* 123 :  (i-1)* 123 + 18
+            let s = i < 7 ? 0 : 5
 
-            // var points = [ 100 + distance, 900, 220 + distance + Offset, 500, 300 + distance, 660  ];
-            var points = [ 100 + distance, 900, 220 + distance , 500, 300 + distance, 660  ];
+            var points = [ (100 + distance), 900,( 220 + distance + Offset), 500 , (300 + distance), 660 + s ]
+            // var points = [ 100 + distance, 900, 220 + distance , 500, 300 + distance, 660  ]
             var curve = new Phaser.Curves.Spline(points);
 
             // var startPoint = new Phaser.Math.Vector2(100 + distance, 900);
@@ -312,36 +352,12 @@
                 }),
                 onComplete:(()=>{
                     _this.ball[i].setScale(.25)
-                    // _this.ball[i].setActive(false)
-                    // _this.ball[i].setFrame(29).setDepth(2)
+                    _this.FBball[i] = _this.add.sprite(_this.ball[i].x,_this.ball[i].y, `FB${no}` ).setScale(.25).setDepth(1)
+                    _this.ball[i].visible =false
 
                 })
             });
 
-
-
-            // _this.ball[i] =  _this.add.image(200 + distance ,  900, `ball${num}`).setScale(.36).setDepth(2)
-            // _this.tween = _this.tweens.add({
-            // targets: _this.ball[i],
-            // props: {
-            //     x: { value: 300 + distance , duration: 1000, flipX: false },
-            //     y: { value: 600, duration: 1000,  },
-            // },
-            //     ease: 'Power1',
-            //     onStart:(()=>{
-            //         console.log('start')
-            //         // _this.ball[i].setScale(.90)
-            //     }),
-            //     onUpdate:(()=>{
-            //         // let num = _this.ball[i]._scaleX
-            //         // num = num -0.001
-            //         // if(num <= 0.7) num = 0.7
-            //         // _this.ball[i].setScale(num)
-            //     }),
-            //     onComplete:(()=>{
-
-            //     })
-            // })
 
             inputAry[i] = false
 
@@ -384,10 +400,15 @@
                                     break;
                     
                             }
+                            // console.log(_this.ball[ index + 1]
                             // sortAry[index] =  _this.add.image( _this.ball[ index + 1].x  , _this.ball[ index + 1].y, `ball${el}`).setScale(.36).setDepth(0)
                             sortAry[index] =  _this.add.sprite( _this.ball[ index + 1].x  , _this.ball[ index + 1].y,  `No${no}`).setScale(.25).setFrame(29).setDepth(0)
+                            _this.FBball[index+1].play('FBball'+ no)
+                            sortAry[index].play('BFball'+ no)
                         })
                         _this.panel.play('rotat1')
+     
+
 
 
                     },1000)
@@ -402,54 +423,13 @@
 
     function update ()
     {
-        // var pointer = this.input.activePointer;
+        var pointer = this.input.activePointer;
 
-        // text1.setText([
-        //     'x: ' + pointer.worldX,
-        //     'y: ' + pointer.worldY,
-        //     // 'isDown: ' + pointer.isDown
-        // ])
-
-       
-        // if(this.go){
-        //     for (let i = 0; i < 7; i++) {
-        //         // console.log( this.test[i])
-        //         this.test[i].setPosition(this.test[i].x - this.R / 8, this.test[i].y + this.R * 5)
-        //         this.test2[i].setPosition(this.test2[i].x - this.R / 8 , this.test2[i].y - this.R * 5).setDepth(0)
-        //     }
-        //     let r = 0
-        //     r +=0.018
-        //     this.R-= r
-
-        //     if(this.R <= 0){
-        //         for (let i = 0; i < 7; i++) {
-        //             this.test2[i].setDepth(0)
-        //             this.test[i].setDepth(0)
-        //         }
-        //         this.go = false
-        //     }
-        // }else{
-        //     if(this.R >= .35){
-        //         return 
-        //         // for (let i = 0; i < 7; i++) {
-        //         //     this.test[i].setDepth(2)
-        //         // }
-        //         // this.go = true
-        //     }
-        //     let r = 0
-        //     r +=0.018
-        //     this.R += r
-        //     for (let i = 0; i < 7; i++) {
-        //         this.test[i].setPosition(this.test[i].x  - this.R / 8, this.test[i].y - this.R * 8).setDepth(0)
-        //         this.test2[i].setPosition(this.test2[i].x  - this.R / 8 , this.test2[i].y + this.R * 8.5).setDepth(2)
-        //     }
-   
-
-        // }
-    
-        
-
-
+        text1.setText([
+            'x: ' + pointer.worldX,
+            'y: ' + pointer.worldY,
+            // 'isDown: ' + pointer.isDown
+        ])
 
 
         if(rotation){
@@ -457,10 +437,10 @@
             if(this.go){
 
                 inputValue.forEach((el,i)=>{
-                    _this.ball[i + 1].setPosition(_this.ball[i + 1].x+ this.R / 8, _this.ball[i + 1].y + this.R * 5 )
+                    _this.FBball[i + 1].setPosition(_this.FBball[i + 1].x+ this.R / 8, _this.FBball[i + 1].y + this.R * 5 )
                 })
                 sortAry.forEach((el,i)=>{
-                    el.setPosition(el.x - this.R / 8 , el.y - this.R * 5 ).setDepth(0)
+                    el.setPosition(el.x + this.R / 8 , el.y - this.R * 5 ).setDepth(0)
                 })
                 
                 let r = 0
@@ -470,22 +450,32 @@
                 if(this.R <= 0) {
                     sortAry.forEach((el,i)=>{
                         el.setDepth(0)
-                        _this.ball[i + 1].setDepth(0)
+                        _this.FBball[i + 1].setDepth(0)
                     })
                     this.go = false
                 }
 
-            }else{
+            }
+            else{
                 if(this.R >= .35 ) return
                 let r = 0
                 r +=0.018
                 this.R += r
 
                 inputValue.forEach((el,i)=>{
-                    _this.ball[i + 1].setPosition(_this.ball[i + 1].x - this.R / 8, _this.ball[i + 1].y - this.R * 8  ).setDepth(0)
+                    // _this.FBball[i + 1].setPosition(_this.FBball[i + 1].x - this.R / 8, _this.FBball[i + 1].y - this.R * 8  ).setDepth(0)
+                    if(i===6){
+                        _this.FBball[i + 1].setPosition(_this.FBball[i + 1].x - this.R / 8, _this.FBball[i + 1].y - this.R * 12  ).setDepth(0)
+                    }else{
+                        _this.FBball[i + 1].setPosition(_this.FBball[i + 1].x - this.R / 8, _this.FBball[i + 1].y - this.R * 8  ).setDepth(0)
+                    }
                 })
                 sortAry.forEach((el,i)=>{
-                    el.setPosition(el.x + this.R / 8, el.y + this.R * 8.5 ).setDepth(2)
+                    if(i===6){
+                        el.setPosition(el.x - this.R / 8, el.y + this.R *  7 ).setDepth(2)
+                    }else{
+                        el.setPosition(el.x - this.R / 8, el.y + this.R * 8.5).setDepth(2)
+                    }
                 })
             }
 
